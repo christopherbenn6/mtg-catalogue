0534e4e1f8d2e5cc19ade53ecd7a26cbd5dbc16b
Christopher Benn <christopherbenn6@gmail.com>
2026-02-21 20:45:35 -0700

    Basic Load More Function

--- js/main.js
+++ js/main.js
@@ -1,20 +1,39 @@
 import { getCardByNameExact, getCardByNameFuzzy, getNewestSetCode, getFromSet, getAllCardsByReleaseDate } from '../js/api.js';
 
-async function loadCardArray() {
-    let cardGrid = document.querySelector('#card-grid')
+let maxNumberOfCards = 200;
+let cardIncrement = 200;
+
+/**
+ * 
+ * @param {int} maxNumberOfCards - The maximum number of cards displayed on the page
+ * @param {int} cardIncrement - The number of cards that are created each button press
+ * @returns 
+ */
+async function loadCardArray(maxNumberOfCards, cardIncrement) {
+    let cardGrid = document.querySelector('#card-grid');
     const cardData = await getAllCardsByReleaseDate();
     const cards = cardData.data;
-    cardData.innerHTML = ``;
+    cardGrid.innerHTML = ``;
 
-    for(let i = 0; i < 30 && i < cards.length; i++) {
-        let image = 'img/blank.avif'
+    for(let i = 0; i < maxNumberOfCards && i < cards.length; i++) {
+        let image = 'img/blank.avif';
         if(cards[i]['card_faces'] != null) {
             image = cards[i]['card_faces'][0]['image_uris']['normal'];
         } else {
             image = cards[i]['image_uris']['normal'];
         }
-        cardGrid.innerHTML += `<img src="${image}">`;
+        cardGrid.innerHTML += `<img class="mtg-card" src="${image}">`;
+    }
+
+    if(cards.length > maxNumberOfCards && document.querySelector('#load-more-button') == null) {
+        const loadMore = document.querySelector('#load-more');
+        const button = `<p id="load-more-button">Load More</p>`;
+        loadMore.innerHTML += button;
+        loadMore.addEventListener('click', () => {
+            maxNumberOfCards += cardIncrement;
+            loadCardArray(maxNumberOfCards);
+        });
     }
 }
 
-loadCardArray();
+loadCardArray(maxNumberOfCards, cardIncrement);

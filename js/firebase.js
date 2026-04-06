7be172d196bf2b1e2edad2cd1d3498735ca08f3a
Christopher Benn <christopherbenn6@gmail.com>
2026-04-06 15:15:03 -0600

    Feature: basic form and sign up form

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+// Firestore
+import {
+  collection,
+  getFirestore,
+  getDocs
+} from "firebase/firestore";
+
+// Authentication
+import { 
+  getAuth, 
+  createUserWithEmailAndPassword 
+} from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +27,36 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
+const db = getFirestore();
+
+// Get form input
+const signUpForm = document.querySelector('#sign-up-form');
+const emailInput = document.querySelector('#email-sign-up');
+const passwordInput = document.querySelector('#password-sign-up');
+const createAccountButton = document.querySelector('#sign-up-with-google');
+const submitButton = document.querySelector('#submit-sign-up');
+
+// Create Account
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  createUserWithEmailAndPassword(auth, emailInput.value, passwordInput.value)
+    .then((userCredential) => {
+      // Signed up 
+      const user = userCredential.user;
+      console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      // ..
+  });
+});
+// CbDa:sdR74
+
+const colRef = collection(db, 'decks');
+getDocs(colRef)
+  .then((snapshot) => {
+    console.log(snapshot.docs)
+  });

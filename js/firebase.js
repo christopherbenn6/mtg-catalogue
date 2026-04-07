30705f7b67e73d9f7972c2494111a35fee0a1cf0
christopherbenn6 <111609212+christopherbenn6@users.noreply.github.com>
2026-04-07 13:15:56 -0600

    Feature: Authenticate with google and log in completed

--- js/firebase.js
+++ js/firebase.js
@@ -11,7 +11,10 @@ import {
 // Authentication
 import { 
   getAuth, 
-  createUserWithEmailAndPassword 
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup  
 } from "firebase/auth";
 
 // Your web app's Firebase configuration
@@ -26,37 +29,137 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Authorization
 const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+// Firestore / DB content
 const db = getFirestore();
 
-// Get form input
+// Get form sign up input
 const signUpForm = document.querySelector('#sign-up-form');
-const emailInput = document.querySelector('#email-sign-up');
-const passwordInput = document.querySelector('#password-sign-up');
-const createAccountButton = document.querySelector('#sign-up-with-google');
-const submitButton = document.querySelector('#submit-sign-up');
+const emailSignUpInput = document.querySelector('#email-sign-up');
+const passwordSignUpInput = document.querySelector('#password-sign-up');
+const signUpWithGoogle = document.querySelector('#sign-up-with-google');
+const signUpButton = document.querySelector('#submit-sign-up');
+
+// Get form sign in input
+const logInForm = document.querySelector('#log-in-form');
+const emailLogInInput = document.querySelector('#email-log-in');
+const passwordLogInInput = document.querySelector('#password-log-in');
+const logInWithGoogleButton = document.querySelector('#log-in-with-google');
+const logInButton = document.querySelector('#submit-log-in');
 
 // Create Account
-submitButton.addEventListener('click', (e) => {
+signUpButton.addEventListener('click', (e) => {
   e.preventDefault();
-  createUserWithEmailAndPassword(auth, emailInput.value, passwordInput.value)
+  createUser(auth, emailSignUpInput.value, passwordSignUpInput.value);
+});
+
+logInButton.addEventListener('click', (e)=> {
+  e.preventDefault();
+  logIn(auth, emailLogInInput.value, passwordLogInInput.value)
+});
+
+// Sign in with google
+logInWithGoogleButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  signInWithGoogle(auth);
+});
+
+// Same as above with other button
+signUpWithGoogle.addEventListener('click', (e) => {
+  e.preventDefault();
+  signInWithGoogle(auth);
+});
+
+/**
+ * 
+ * @param {string} tableName - The name of the table to fetch from
+ * @returns {Promise} - array of data as a promise
+ */
+function getDataArray(tableName) {
+  let dataArray = []
+  const colRef = collection(db, tableName);
+  return getDocs(colRef)
+  .then((snapshot) => {
+    snapshot.docs.forEach((doc) => {
+      dataArray.push({ ...doc.data(), id: doc.id}) 
+    });
+    return dataArray;
+  });
+}
+
+/**
+ * 
+ * @param {object} auth -- Authentication Instance
+ * @param {*} email -- User Input Email
+ * @param {*} password -- User Input Password 
+ */
+function createUser(auth, email, password) {
+  createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // Signed up 
       const user = userCredential.user;
-      console.log(user);
+      console.log("Signed Up");
       // ...
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
       console.log(errorMessage);
-      // ..
   });
-});
-// CbDa:sdR74
+}
 
-const colRef = collection(db, 'decks');
-getDocs(colRef)
-  .then((snapshot) => {
-    console.log(snapshot.docs)
+/**
+ * 
+ * @param {object} auth -- Authentication Instance
+ * @param {*} email -- User Input Email
+ * @param {*} password -- User Input Password 
+ */
+function logIn(auth, email, password) {
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log("Logged In");
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorMessage);
   });
+}
+
+/**
+ * This function works for both signing in and signing up.
+ * @param {object} auth 
+ */
+function signInWithGoogle (auth) {
+  signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    console.log("Logged in with Google");
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    console.log(errorMessage);
+    // ...
+  });
+}
+
+let data = await getDataArray('decks');
+console.log(data);
